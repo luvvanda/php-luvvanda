7bccb94e7e55292f7cc1d47920a546ae9709f62f
lavi <katyaleet133723@gmail.com>
2024-02-07 23:17:20 +0300

    task1

---
+++ LAB1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomeWork1</title>
+</head>
+<body>
+    <header>
+        <div class = "header__wrapper">
+            <img class= "header__logo" src="./img/logo.png" alt="">
+            <h1 class = "header__h">Hellow, World!</h1>
+        </div>
+    </header>
+    <main>
+        <?php
+            $greetings = '<p> Hello, World! </p>';
+            echo $greetings;
+        ?>
+    </main>
+    <footer>
+        <p>Апаткина Екатерина 231-321</p>
+    </footer>
+</body>
+</html>
